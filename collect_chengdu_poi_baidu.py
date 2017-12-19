c8c62e2529137c943cfd0989768e3a6deb84f05a
KYH <kkyyhh96@163.com>
2017-12-19 19:16:58 +0800

    baidu collect poi change1

--- collect_chengdu_poi_baidu.py
+++ collect_chengdu_poi_baidu.py
@@ -61,6 +61,32 @@ class BaiduAPI(object):
         self.bounds = bounds
 
     # Each search request
+    def search_poi_first(self, bound):
+        try:
+            params = {
+                "q": str(self.word),
+                "page_size": 20,
+                "page_num": 0,
+                "scope": 2,
+                "coord_type": 2,
+                "ret_coordtype": "gcj02ll",
+                "ak": self.api_key,
+                "output": "json",
+                "bounds": bound
+            }
+            try:
+                r = requests.get("http://api.map.baidu.com/place/v2/search", params)
+                json_data = json.loads(r.text)
+                if json_data['status'] == 0:
+                    return json_data['total'],json_data
+            # Translate json file
+            except Exception as exception:
+                with open("log.txt", 'a') as log:
+                    log.writelines(exception)
+        except Exception as e:
+            return None
+
+    # Each search request
     def search_poi(self, page_num, bound):
         try:
             params = {
@@ -69,25 +95,27 @@ class BaiduAPI(object):
                 "page_num": page_num,
                 "scope": 2,
                 "coord_type": 2,
-                "ret_coordtype":"gcj02ll",
+                "ret_coordtype": "gcj02ll",
                 "ak": self.api_key,
                 "output": "json",
                 "bounds": bound
             }
             try:
                 r = requests.get("http://api.map.baidu.com/place/v2/search", params)
-                # Translate json file
                 json_data = json.loads(r.text)
+                if json_data['status'] == 0:
+                    return json_data
+            # Translate json file
             except Exception as exception:
                 with open("log.txt", 'a') as log:
                     log.writelines(exception)
+        except Exception as e:
+            return None
 
-            # No result then next request
-            if json_data['total'] == 0:
-                return False
-            # Have result then get the name and coordinates
-            else:
-                for location in json_data['results']:
+    def decode_json(self, json_data):
+        if json_data is not None:
+            for location in json_data['results']:
+                try:
                     poi = POI(location['name'], location['location']['lat'], location['location']['lng'],
                               location['address'])
                     type = ""
@@ -102,19 +130,25 @@ class BaiduAPI(object):
                         pass
                     poi.get_type(type, tag)
                     poi.poi_write()
-                    return True
-        except Exception as e:
-            return False
+                except Exception as e:
+                    with open("log.txt", 'a') as log:
+                        log.writelines(e)
 
     # Get all poi data in the bound
     def get_all_poi(self):
         for bound in self.bounds:
             try:
-                count = 0
-                while count < 20 and self.search_poi(count, bound) == True:
-                    count += 1
-                else:
-                    continue
+                count, json_data = self.search_poi_first(bound)
+                page_count = int(count / 20)
+                self.decode_json(json_data)
+                if page_count > 0:
+                    for i in range(1, page_count+1):
+                        try:
+                            json_data = self.search_poi(count, bound)
+                            self.decode_json(json_data)
+                        except Exception as e:
+                            with open('log.txt', 'a') as logfile:
+                                logfile.writelines(e)
             except Exception as e:
                 with open('log.txt', 'a') as logfile:
                     logfile.writelines(e)
